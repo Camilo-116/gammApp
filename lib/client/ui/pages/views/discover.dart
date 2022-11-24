05ee1986ff8116139e8bc69f0405fcc5ebad13a8
Sebastian Enrique Gonzalez Benítez <gonzalezes@uninorte.edu.co>
2022-11-24 18:29:05 -0500

    Update discover.dart

--- lib/client/ui/pages/views/discover.dart
+++ lib/client/ui/pages/views/discover.dart
@@ -1,7 +1,5 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gamma/server/models/user_model.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
